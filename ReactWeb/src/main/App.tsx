7bb56906f41930ddf4dfeddd96c2efb66b0a1c30
Rafael Humberto Cárdenas <Rafael.Cardenas@Chubb.com>
2025-02-27 12:05:21 -0600

    React And Api Integracion

---
+++ ReactWeb/src/main/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Header from './Header'
+
+function App() {
+
+  return (
+    <div className='container'>
+      <Header subtitle='Providing houses all over the world' />
+    </div>
+  )
+}
+
+export default App
